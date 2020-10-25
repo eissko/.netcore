4973f38a2c52c19a3af24932fd6d44d0c60ff3a8
eissko <eissko@gmail.com>
2020-10-25 19:31:02 +0100

    ad search using system.directoryservices lib

---
+++ activedirectory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Configuration;
+
+
+namespace activedirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var computerName = "comp1";
+
+            string server = ConfigurationManager.AppSettings.Get("server");
+            string userName = ConfigurationManager.AppSettings.Get("userName");
+            string password = ConfigurationManager.AppSettings.Get("password");
+            string domain = ConfigurationManager.AppSettings.Get("domain");
+
+            var directoryContext = GetDirectoryContext(server,domain,userName, password);
+            var result = FindComputer(computerName,directoryContext);
+            var directoryEntry = result.GetDirectoryEntry();
+            
+            Console.WriteLine(directoryEntry.SchemaClassName);
+            RemoveComputer(directoryEntry);
+        }
+
+        public static DirectoryEntry GetDirectoryContext(string serverFQDN, string distinguishedName, string userName, string password ){
+            return new DirectoryEntry("LDAP://"+serverFQDN+"/"+ distinguishedName, userName, password);
+        }
+        
+        public static void CreateComputer(string computerName, DirectoryEntry directoryEntry){
+
+        }
+        public static System.DirectoryServices.SearchResult FindComputer(string computerName, DirectoryEntry directoryEntry)
+        {
+                var searcher    = new DirectorySearcher(directoryEntry);
+                searcher.Filter = string.Format("(&(objectClass=computer)(CN={0}))", computerName);
+                searcher.PropertiesToLoad.Add("distinguishedName");
+                var result = searcher.FindOne();
+                Console.WriteLine(result.GetType());
+                return result;
+        }
+
+        public static void RemoveComputer(System.DirectoryServices.DirectoryEntry directoryEntry){
+            if(directoryEntry.SchemaClassName == "computer"){
+                Console.WriteLine("true");
+            }
+            else{
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
