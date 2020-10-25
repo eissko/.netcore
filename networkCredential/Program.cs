8130f7069fadc352338a6153f7f22b2753970b4d
eissko <eissko@gmail.com>
2020-10-25 19:33:30 +0100

    add readme.md & networkCredentials example

---
+++ networkCredential/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace networkCredential
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string userName = "user1";
+            string password = "Pwd1";
+            NetworkCredential credentials = new NetworkCredential(userName,password);
+            Console.WriteLine(string.Format("Password is: {0}", credentials.Password));
+        }
+    }
+}
